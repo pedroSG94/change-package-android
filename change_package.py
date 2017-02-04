ed4deb9a3c1392cb9c630e2b08f7aca05ad6c962
Pedro <pedrossgg1994@gmail.com>
2017-02-04 18:54:37 +0100

    work on windows

--- change_package.py
+++ change_package.py
@@ -5,8 +5,8 @@ old_package = sys.argv[1]
 new_package = sys.argv[2]
 
 def show_arguments():
-	print("old package: " + old_package)
-	print("new package: " + new_package)
+	print("Old package: " + old_package)
+	print("New package: " + new_package)
 
 def replace_text(path_file, old_text, new_text):
 	f = open(path_file, "r")
@@ -23,19 +23,20 @@ def list_folder(path_folder):
 	for f in os.listdir(path_folder):
 		print("file " + str(f))
 
-		#es una carpeta
+		#is a folder
 		if os.path.isdir(path_folder + "/" + f):
-			abs_path = os.path.abspath(path_folder + "/" + str(f))
+			abs_path = os.path.abspath(path_folder + os.sep + str(f))
 			if(str(f) != "build"):
 				list_folder(abs_path)
 			for i in range(len(old_package.split("."))):
 				if(str(f) == old_package.split(".")[i]):
-					os.rename(path_folder + "/" + str(f), path_folder + "/" + new_package.split(".")[i])
+					os.rename(path_folder + os.sep + str(f), path_folder + os.sep + new_package.split(".")[i])
 
-		#es un fichero que no es el mismo script
-		elif str(f) != "files.py":
+		#is a file
+		else:
+			#only change java and xml files
 			if(str(f).endswith(".java") or str(f).endswith(".xml")):
-				replace_text(path_folder + "/" + str(f), old_package, new_package)
+				replace_text(path_folder + os.sep + str(f), old_package, new_package)
 			else:
 				print("ignore this file")
 
